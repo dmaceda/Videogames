435e5d42b8f0961422f9fff69fa578c699e64d03
Diego Maceda <dmaceda2288@gmail.com>
2022-12-30 17:56:07 -0300

    replace deploy url

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import {store} from './store';
-import axios from 'axios';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 
-
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+axios.defaults.baseURL =
+  "videogames-production-2800.up.railway.app" || "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </Provider>,
-  document.getElementById('root')
-
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
